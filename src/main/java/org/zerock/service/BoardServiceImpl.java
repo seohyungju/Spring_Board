cbfe7832458a03d2755d905e1810d84eb4477a83
SEO <shj890917@naver.com>
2018-06-23 22:41:12 +0900

    register.jsp : commponent-scan path error modification

---
+++ src/main/java/org/zerock/service/BoardServiceImpl.java
@@ -0,0 +1,43 @@
+package org.zerock.service;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.springframework.stereotype.Service;
+import org.zerock.domain.BoardVO;
+import org.zerock.persistence.BoardDAO;
+
+@Service
+public class BoardServiceImpl implements BoardService{
+
+	@Inject
+	private BoardDAO dao;
+	
+	@Override
+	public void regist(BoardVO board) throws Exception {
+		dao.create(board);
+	}
+
+	@Override
+	public BoardVO read(Integer bno) throws Exception {
+		//mybatis xml 의 resultType 이 BoardVO 이기 때문에 형변환 하지 않아도 된다.
+		return dao.read(bno);
+	}
+
+	@Override
+	public void modify(BoardVO board) throws Exception {
+		dao.update(board);
+	}
+
+	@Override
+	public void remove(Integer bno) throws Exception {
+		dao.delete(bno);
+	}
+
+	@Override
+	public List<BoardVO> listAll() throws Exception {
+		return dao.listAll();
+	}
+	
+}
